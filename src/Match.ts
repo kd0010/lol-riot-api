d92f084f14fef107e2a49708031fbc0db303a600
David Kander <kanders.davids@gmail.com>
2023-05-17 19:36:04 +0300

    add methods to `Match`

--- src/Match.ts
+++ src/Match.ts
@@ -1,8 +1,10 @@
+import { ChampionId } from 'lol-constants'
 import {
   IMatch,
   MatchMetadata,
   MatchResponse,
 } from './types/interfaces'
+import { MatchParticipant } from './types/match'
 
 export class Match {
   data: IMatch
@@ -14,5 +16,28 @@ export class Match {
     this.data = data.info
     this.metadata = data.metadata
     this.raw = data
+  }
+
+  getChampionIdBySummonerName(
+    summonerName: string,
+  ): ChampionId | null {
+    for (const {
+      championName,
+      summonerName: summName,
+    } of this.data.participants) {
+      if (summName != summonerName) continue
+      return championName
+    }
+    return null
+  }
+
+  getParticipantBySummonerName(
+    summonerName: string,
+  ): MatchParticipant | null {
+    for (const participant of this.data.participants) {
+      if (participant.summonerName != summonerName) continue
+      return participant
+    }
+    return null
   }
 }
